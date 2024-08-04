9305b0ec50cedca1ad49210cdcbaf2d74e240047
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-08-04 15:15:58 -0500

    Apagamos en AppServiceProvided las propiedades protegidas para que pasen los tests

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,8 +11,9 @@ class Job extends Model
 {
     use HasFactory;
 
-    public function tag() {
-
+    public function tag(string $name) {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+        $this->tags()->attach($tag);
     }
 
     public function tags(): BelongsToMany {
