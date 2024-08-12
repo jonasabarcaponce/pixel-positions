1f0f6aaa5cb0262b598d4a913964a93afdf61a5b
Jonas Abarca Ponce <jonasabarcaponce@gmail.com>
2024-08-12 12:06:34 -0500

    Function to Group by Featued Jobs

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -5,9 +5,9 @@
         <x-employer-logo />
     </div>
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div>
         @foreach ($job->tags as $tag)
